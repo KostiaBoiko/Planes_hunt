dd8e16582c810518a0a031c59af9db6ac2032e24
goldi4ek <2002mdo@gmail.com>
2023-02-24 13:51:38 +0200

    created class game and it's main methods

---
+++ game.py
@@ -0,0 +1,53 @@
+import pygame
+import settings
+from random import randint
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(settings.RESOLUTION, flags=pygame.SCALED)
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption('Z-Plane Hunt')
+        self.mouse = 0, 0
+        self.timer = 60
+
+    def refresh(self):
+        pygame.display.flip()
+        self.clock.tick(settings.FPS)
+        self.mouse = pygame.mouse.get_pos()
+        self.score_bar_text = self.font.render(f"Score: {self.score}", True, "White")
+
+    def draw(self):
+        self.screen.blit(self.background, (0, 0))
+        self.screen.blit(self.score_bar, (settings.WIDTH - 188, settings.HEIGHT - 64))
+        self.screen.blit(self.score_bar_text, (settings.WIDTH - 170, settings.HEIGHT - 42))
+        self.screen.blit(self.cursor, self.mouse)
+
+    @staticmethod
+    def check_events():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+    def gameplay(self):
+        if self.timer > 0:
+            self.timer -= 1
+        else:
+            enemy.Enemy(self)
+            self.timer = randint(30, 50)
+
+        # updating model position
+        for target in enemy.enemies:
+            target.update(self)
+
+        # drawing the model
+        for target in enemy.enemies:
+            target.draw(self)
+
+    def run(self):
+        while True:
+            self.check_events()
+            self.draw()
+            self.gameplay()
+            self.refresh()
