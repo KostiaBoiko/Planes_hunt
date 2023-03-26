1cd58087ef8bf3ad58581062a65f58819e37dfa4
goldi4ek <2002mdo@gmail.com>
2023-03-26 14:26:05 +0300

    added and edited some tests

--- tests/test_programm.py
+++ tests/test_programm.py
@@ -14,11 +14,7 @@ def enemy(game):
     return Enemy(game)
 
 
-@pytest.fixture()
-def menu():
-    return Menu()
-
-
+@pytest.mark.game
 def test_game(game):
     result = game.score
     assert result == 0
@@ -38,3 +34,17 @@ def test_enemy_change_position(enemy, a, b, expected_result):
 def test_enemy_reduce_points(enemy, a, expected_result):
     result = enemy.reduce_points(a)
     assert result == expected_result
+
+
+@pytest.mark.parametrize("a, expected_result", [(0, 1),
+                                                (9, 10),
+                                                (10, 11)])
+def test_enemy_append_points(enemy, a, expected_result):
+    result = enemy.append_points(a)
+    assert result == expected_result
+
+
+def test_enemy_type(enemy):
+    plane_types = [0, 1, 2]
+    result = enemy.plane_type
+    assert result in plane_types
